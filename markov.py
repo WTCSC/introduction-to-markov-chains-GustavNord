befc955325d1b09e777ae9ee1838be3bb9162221
GustavNord <gus.nordqvist@gmail.com>
2024-11-12 15:43:08 -0700

    Some Updates to Comments in the markov.py

--- markov.py
+++ markov.py
@@ -1,11 +1,14 @@
 # Comments
 import random
 
+# Asks for user input for a starting word and how many words to be created
 word_input = input("Starting Word: ")
 number_input = int(input("Number of Words: "))
 
+# Defines what the source should be
 text = "It is always a pleasure to a man in public life to meet the real governing classes. I wish to bid you welcome to Washington this evening, and to say but one word of greeting to you, and that word shall take the form of a warning. I did not speak in jest when I alluded to you as representatives of the governing classes. I think that we of the United States can not keep too fresh in our minds the fact that the men ultimately responsible for the Government are not the representatives of the people, but the people themselves, and that therefore heavy is the responsibility that lies upon the people and above all upon those who do the most toward shaping the thought of the people. In the days of my youth I was a literary man myself. In reading a book recently, a series of essays, I was immensely struck by one thought developed in it. The writer, one of our greatest scholars, was speaking of the fact that freedom could not exist unless there went with it a thorough appreciation of responsibility,[6] and he used a phrase somewhat like this—that among all peoples there must be restraint; if there is no restraint the result is inevitably anarchy. That means the negation of all government, and the negation of all government of course means the negation of popular government; and that therefore there must be restraint, and that therefore a free people had merely substituted self-restraint for external restraint; and the permanence of our freedom as a people, the permanence of our liberties, depends upon the way in which we show and exercise that self-restraint"
 
+# Replaces the periods from the taken sources with nothing
 new_text = text.replace(".", "")
 
 transitions = {}
